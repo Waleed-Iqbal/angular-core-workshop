2db911cdb2563404f1d93b6783acff3c7f976d74
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-02-25 18:43:09 +0500

    Customers component added

--- apps/dashboard/src/app/customers/customers.module.ts
+++ apps/dashboard/src/app/customers/customers.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
+import { CustomersComponent } from './customers.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CustomersComponent],
   imports: [
     CommonModule,
     CustomersRoutingModule
